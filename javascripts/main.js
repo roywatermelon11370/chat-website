453ccd1df701a30efe4e730e762496d92b322318
roywatermelon11370 <53699625+roywatermelon11370@users.noreply.github.com>
2020-04-14 00:15:32 +0800

    1

---
+++ javascripts/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  // navbar
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop()) {
+      $('#navbar').addClass('shadow');
+    } else {
+      $('#navbar').removeClass('shadow');
+    }
+  });
+
+  // backtotop
+  $('#backToTop').click(function (e) {
+    e.preventDefault();
+    $('html,body').stop().animate({
+      scrollTop: 0
+    }, 300, 'swing')
+  });
+
+  // sidebar 
+  sidebarToggle = function() {
+    $('#sidebar').toggleClass('sidebar-active').toggleClass('sidebar-hide');
+    $('#bg-dark').toggleClass('hidden').toggleClass('block');
+  }
+
+  $('.h-screen-nav').css('height', function(){
+    var win = $(window).height();
+    var navv = $('#navbar').height();
+    var siz = win - navv;
+    return siz;
+  });
+
+});
